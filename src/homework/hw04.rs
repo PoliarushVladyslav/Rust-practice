538601974d3b2cf6a9c8c01f90fb41190ade79b5
EsgalGollor <71118184+EsgalGollor@users.noreply.github.com>
2025-03-26 19:49:15 +0200

    Task 2

---
+++ src/homework/hw04.rs
@@ -0,0 +1,19 @@
+const WIDTH: usize = 11; // Ширина конверта
+const HEIGHT: usize = 6; // Висота верхньої половини
+
+fn main() {
+    let mut output = String::new();
+
+    for i in 0..HEIGHT {
+        output.push_str(&" ".repeat(HEIGHT - i - 1));
+        output.push_str(&"*".repeat(2 * i + 1));
+        output.push('\n');
+    }
+    for i in (0..HEIGHT - 1).rev() {
+        output.push_str(&" ".repeat(HEIGHT - i - 1));
+        output.push_str(&"*".repeat(2 * i + 1));
+        output.push('\n');
+    }
+
+    print!("{}", output);
+}
