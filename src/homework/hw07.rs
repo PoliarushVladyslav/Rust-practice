9e0ed55ffbbccb9009970ca58e7fd0bf521cae9d
PoliarushVladyslav <131451695+PoliarushVladyslav@users.noreply.github.com>
2025-05-02 23:08:20 +0300

    Update hw07.rs

---
+++ src/homework/hw07.rs
@@ -0,0 +1,19 @@
+fn invert_the_case(s: String) -> String {
+    s.chars()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
+            } else {
+                c.to_string()
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let input = String::from("Hello");
+    let output = invert_the_case(input);
+    println!("{}", output);
+}
